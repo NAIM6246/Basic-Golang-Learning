ff751d60f157f1d265bd9b7a1ca9d96307f9af4d
Naim <uddinnaim895@gmail.com>
2020-12-29 00:16:29 +0600

    Using handle to route with go-chi module

--- Server.go
+++ Server.go
@@ -1,34 +1,18 @@
 package main
 
 import (
+	"Golang/handler"
 	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
 )
 
 func main() {
 	port := ":3000"
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Get("/get", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome\n"))
-		fmt.Fprintf(w, "Data Fetching")
-	})
-
-	r.Post("/post", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Your data is posted"))
-	})
-
-	r.Put("/put", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Data updated")
-	})
-
-	r.Delete("/delete", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Data is deleted")
-	})
-
+	rout := chi.NewRouter()
+	userHandler := handler.NewUserHandler()
+	rout.Route("/users", userHandler.Handle)
 	fmt.Println("Serving on port ", port)
-	http.ListenAndServe(port, r)
+	http.ListenAndServe(port, rout)
 }
